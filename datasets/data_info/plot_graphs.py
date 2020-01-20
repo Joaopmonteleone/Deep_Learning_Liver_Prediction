876995e72c7fbf69bf052d21c9c9ac5d02042995
Maria Luque Anguita <mluqueanguita@gmail.com>
2020-01-20 11:56:50 +0000

    cumulative frequency graphs

---
+++ datasets/data_info/plot_graphs.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 11:29:41 2020
+
+@author: Maria
+"""
+
+# CUMULATIVE FREQUENCY
+import numpy as np
+import matplotlib.pyplot as plt
+
+# read data
+data = []
+with open('honours_dataset_2.csv') as csvfile:
+   readCSV = csv.reader(csvfile, delimiter=',')
+   for row in readCSV:
+      data.append(row[39])
+      
+data.pop(0)
+data = list(map(int, data))
+      
+# evaluate the histogram
+values, base = np.histogram(data, bins='auto')
+#evaluate the cumulative
+cumulative = np.cumsum(values)
+# plot the cumulative function
+plt.plot(base[:-1], cumulative)
+
+plt.show()
+
