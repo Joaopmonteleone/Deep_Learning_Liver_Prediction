4da5d2defa056877f29c2c44ad32f09b6f9c3c2c
Maria Luque Anguita <mluqueanguita@gmail.com>
2020-02-02 13:08:07 +0000

    pca to dataset

--- liver_prediction_ANN.py
+++ liver_prediction_ANN.py
@@ -55,14 +55,16 @@ X = dataset.iloc[:, :-2].values # all rows, all columns except last result and 3
 y_before = dataset.iloc[:, 39].values # all rows, last column (result) keep a record to compare later
 
 # Encoding categorical data
+# output NOT encoded
+y = y_before
 # encoding the output
 onehotencoder = OneHotEncoder(categorical_features = [39])
 y = onehotencoder.fit_transform(dataset.values).toarray()
 y = y[:, 0:4]
 # encoding the input
 onehotencoder = OneHotEncoder(categorical_features = [6, 7, 14, 21, 36]) 
-# etiology, portal thrombosis, pretransplant status performance, cause of death, cold ischemia time 
 X = onehotencoder.fit_transform(X).toarray()
+# etiology, portal thrombosis, pretransplant status performance, cause of death, cold ischemia time 
 
 # Separating each column to predict separate classes
 y_1 = y[:, 0]
@@ -71,7 +73,7 @@ y_3 = y[:, 2]
 y_4 = y[:, 3]
 
 # Splitting the dataset into the Training set and Test set
-X_train, X_test, y_train, y_test = train_test_split(X, y_1, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 ''' test_size is 20% = 0.2
     random_state is a generator for random sampling '''
 
@@ -227,6 +229,66 @@ best_accuracy = grid_search.best_score_
 
 
 
+
+###############################################
+#                     PCA                     #
+###############################################
+
+# Applying PCA
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 2)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+explained_variance = pca.explained_variance_ratio_
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue', 'yellow')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green', 'blue', 'yellow'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue', 'yellow')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green', 'blue', 'yellow'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
 
 
 
