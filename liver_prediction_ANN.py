097e468d2d3014a7484c6b32d1eefa1fc29cbb08
Maria Luque Anguita <mluqueanguita@gmail.com>
2020-02-03 12:14:33 +0000

    regression dataset
    testing with grid search 77% accurate in predicting good matches

--- liver_prediction_ANN.py
+++ liver_prediction_ANN.py
@@ -73,7 +73,7 @@ y_3 = y[:, 2]
 y_4 = y[:, 3]
 
 # Splitting the dataset into the Training set and Test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y_4, test_size = 0.2, random_state = 0)
 ''' test_size is 20% = 0.2
     random_state is a generator for random sampling '''
 
@@ -115,20 +115,19 @@ def neuralNetwork():
    classifier.add(Dropout(rate=0.1))
    
    # Adding the output layer
-   classifier.add(Dense(units = 4, kernel_initializer = 'uniform', activation = 'softmax'))
+   classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
    # softmax for classification
    # sigmoid or binary
    
    # Compiling the ANN
-   classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+   classifier.compile(optimizer = 'adagrad', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
    # Fitting the ANN to the Training set
-   classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+   classifier.fit(X_train, y_train, batch_size = 10, epochs = 500) 
  
    return classifier
 
 
-#classifier = KerasClassifier(build_fn = neuralNetwork)
 classifier = neuralNetwork()
 
 
@@ -168,16 +167,18 @@ cm = confusion_matrix(y_test, y_bool)
 #             Evaluating the ANN              #
 ###############################################
 
-
-def build_classifier():
+def ann_architecture():
     classifier = Sequential()
     classifier.add(Dense(units = 30, kernel_initializer = 'uniform', activation = 'relu', input_dim = 55))
     classifier.add(Dense(units = 30, kernel_initializer = 'uniform', activation = 'relu'))
-    classifier.add(Dense(units = 4, kernel_initializer = 'uniform', activation = 'sigmoid'))
-    classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = 'adagrad', loss = 'binary_crossentropy', metrics = ['accuracy'])
     return classifier
 
-classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, epochs = 100)
+classifier = KerasClassifier(build_fn = ann_architecture, batch_size = 10, epochs = 500)
+
+''' Same as the ANN architecture but instead of fitting to X_Train and y_train, it is vuilt
+    on 10 different training folds, each time measuring the model performance on one test fold. ''' 
 
 # this line takes a while
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = 1)
@@ -185,12 +186,19 @@ accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, c
     X = the data to fit (X_train)
     y = to train a model, you need y's to understand correlations
     cv: number of folds in k-fold cross validation, 10 is recommended
-    n_jobs: number of CPUs to use to do the computations, -1 means 'all CPUs'  '''
+    n_jobs: number of CPUs to use to do the computations, -1 means 'all CPUs' to run parallel
+        computations and run the training faster'''
 
 mean = accuracies.mean() # find the average of the accuracies
 variance = accuracies.std() # find the variance of the accuracies (if < 1% = rather low variance)
 
+# WITH BALANCED DATASET CATEGORICAL
+# mean 0.3441 - very very very low
+# variance 0.08 is < 1% so good 
 
+# WITH BALANCED DATASET BINARY - 4
+# mean 0.77%
+# variance 0.07%
 
 
 
@@ -198,24 +206,25 @@ variance = accuracies.std() # find the variance of the accuracies (if < 1% = rat
 ###############################################
 #        Improving & Tuning the ANN           #
 ###############################################
-'''
-    Dropout Regularization to reduce overfitting 
-    PARAMETER TUNING - THE GRID SEARCH TECHNIQUE
-    When tuning the optimizer, the parameters to study must be passed through the function
-'''
+''' PARAMETER TUNING - THE GRID SEARCH TECHNIQUE
+    When tuning the optimizer, the parameters to study must be passed through the function 
+    Classification metrics can't handle a mix of multilabel-indicator and multiclass targets
+    ONLY USE WHEN PREDICTING BINARY OUTPUTS '''
 
-def build_classifier(optimizer):# optimizer is passed because it is tuned in the parameters
+def build_classifier(optimizer, units):# optimizer is passed because it is tuned in the parameters
     classifier = Sequential() # this is a local classifier
-    classifier.add(Dense(units = 28, kernel_initializer = 'uniform', activation = 'relu', input_dim = 55))
-    classifier.add(Dense(units = 28, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = units, kernel_initializer = 'uniform', activation = 'relu', input_dim = 55))
+    classifier.add(Dense(units = units, kernel_initializer = 'uniform', activation = 'relu'))
     classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
     classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
     return classifier
 
-classifier = KerasClassifier(build_fn = build_classifier) # the global classifier 
+classifier = KerasClassifier(build_fn = build_classifier) 
 parameters = {'batch_size': [10, 25, 32, 40, 50], #10
-              'epochs': [100, 500, 750], #500
-              'optimizer': ['adam', 'rmsprop', 'sgd', 'adagrad']} # adagrad
+              'epochs': [100, 500], #500
+              'optimizer': ['adam', 'rmsprop', 'sgd', 'adagrad'], #adagrad
+              'units': [5, 15, 30, 45]
+              } 
 
 grid_search = GridSearchCV(estimator = classifier,
                            param_grid = parameters,
