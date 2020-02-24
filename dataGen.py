7deae5a0bb2cd324c2ba33d708df989868241c07
Maria Luque Anguita <mluqueanguita@gmail.com>
2020-02-24 13:38:20 +0100

    data generation script finished

--- dataGen.py
+++ dataGen.py
@@ -4,51 +4,31 @@ Created on Fri Feb 21 12:16:40 2020
 
 @author: Maria
 """
-
+import numpy as np
 import pandas as pd
+import csv
+
 dataset = pd.read_csv('datasets/regAll.csv')
 
-X_before = dataset.iloc[:, :-1] # all rows, all columns except last result and 3 months answer - (1198, 39)
-y_before = dataset.iloc[:, (dataset.values.shape[1]-1)].values # all rows, last column (result) keep a record to compare later
-
-import numpy as np
-
-age = pd.DataFrame(np.random.randint(15,77,size=(1000, 1)), columns=list('a'))
+# make a dataframe of age so the other columns can be added later on
+age = pd.DataFrame(np.random.randint(15,77,size=(8000, 1)), columns=list('a'))
 age = age.rename(columns={'a':'age'})
 
 global concatenated
 
+# make a list of all column names and remove 'age' from it
 columnNames = []
-for col in X_before.columns:
+for col in dataset.columns:
     columnNames.append(col)
 columnNames.remove('age')
 
+# for ecery column, create 8000 rows of random numbers between that column's max and min values
 for col in columnNames:
-    df = pd.DataFrame(np.random.randint((X_before[col].min()),(X_before[col].max()+1),size=(1000, 1)), columns=list('a'))
-    df = df.rename(columns={'a':col})
-    concatenated = pd.concat([age, df], axis=1)
+    df = pd.DataFrame(np.random.randint((dataset[col].min()),(dataset[col].max()+1),size=(8000, 1)), columns=list('a'))
+    df = df.rename(columns={'a':col}) # rename column name to what it is
+    age = pd.concat([age, df], axis=1) # join lists
 
+dataset = dataset.append(age, ignore_index = True, sort=False) # add synthetic data to original data
 
-gender = pd.DataFrame(np.random.randint(0,2,size=(1000, 1)), columns=list('a'))
-gender = gender.rename(columns={'a':'gender'})
-
-bmibasal = pd.DataFrame(np.random.randint(14,70,size=(1000, 1)), columns=list('a'))
-bmibasal = bmibasal.rename(columns={'a':'bmibasal'})
-
-diabetesPreTx = pd.DataFrame(np.random.randint(0,2,size=(1000, 1)), columns=list('a'))
-diabetesPreTx = diabetesPreTx.rename(columns={'a':'diabetesPreTx'})
-
-gender = pd.DataFrame(np.random.randint(0,2,size=(1000, 1)), columns=list('a'))
-gender = gender.rename(columns={'a':'gender'})
-
-gender = pd.DataFrame(np.random.randint(0,2,size=(1000, 1)), columns=list('a'))
-gender = gender.rename(columns={'a':'gender'})
-
-gender = pd.DataFrame(np.random.randint(0,2,size=(1000, 1)), columns=list('a'))
-gender = gender.rename(columns={'a':'gender'})
-
-concatenated = pd.concat([age, gender, bmibasal,
-                          diabetesPreTx], 
-                          axis=1)
-
-X_before = X_before.append(concatenated, ignore_index = True, sort=False)
+dataset.to_csv(r'C:\Users\Maria\Desktop\Deep_Learning_Liver_Prediction\datasets\syntheticDataReg.csv',
+               index = False)
